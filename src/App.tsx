7bf2f8a14d45f882e32f018d851e0989c8c4dda7
Alan Borges <alanborgesdev0@gmail.com>
2026-08-06 14:21:16 -0300

    feat: adiciona o componente Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 function App() {
-    return <>
-        <Header/>
-    </>;
+    return (
+        <>
+            <Header />
+
+            <main className="py-10">
+                <Hero />
+            </main>
+        </>
+    );
 }
 
 export default App;
